19e51818218210d27b0a9664f7db18f0c613f3f6
garvit <garvit1608@gmail.com>
2016-07-20 20:04:18 +0530

    request action changed

--- actions/actions.js
+++ actions/actions.js
@@ -1,11 +1,21 @@
+import $ from 'jquery';
+import {
+	GET_RESULT
+} from './actionTypes.js';
 
-export const getSearchResult = (dispatch) => {
+export const getSearchResult = (dispatch, q) => {
+	$('#loading').show();
   $.ajax({
       method: "GET",
-      url: "/api/data",
-      dataType: "json"
-    }).success(function(data){
-      return dispatch({type:'GET_BOOK', data: data});
+      url: "https://api.github.com/search/users?q="+q,
+      dataType: "json",
+      success: (data) => {
+      	$('#loading').hide();
+      	return dispatch({type: GET_RESULT, data: data});
+      },
+      error: () => {
+      	$('#loading').hide();
+      }
     });
 };
 
