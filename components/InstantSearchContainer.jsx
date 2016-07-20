48b1c59c3d27d50a39fdbdeb0f320f284d58fb30
garvit <garvit1608@gmail.com>
2016-07-20 16:23:08 +0530

    instant search container component added

---
+++ components/InstantSearchContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class InstantSearchContainer extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<SearchBox
+			/>
+			<SearchResult
+			/>
+		)
+	}
+}
