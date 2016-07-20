e3fec53b20d86d5c7bc146849d07da55d22e5dec
garvit <garvit1608@gmail.com>
2016-07-20 17:17:50 +0530

    store config added

---
+++ main.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import search from './reducers/index.js';
+import App from './App.jsx';
+
+const initialState = {
+	searchResults: [],
+	memoizedResults: []
+}
+
+const reducers = combineReducers({
+	searchResults: search.getSearchResults,
+	memoizedResults: search.getMemoizedResults
+});
+
+let store = createStore(reducers,	initialState);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
