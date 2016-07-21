5297c90eb58d4fee40933837b24f71b2a3b341b5
garvit <garvit1608@gmail.com>
2016-07-21 15:14:43 +0530

    tried merging search result and memoized component

--- components/SearchResult.jsx
+++ components/SearchResult.jsx
@@ -5,15 +5,26 @@ class SearchResult extends React.Component {
 
 	constructor(props) {
 		super(props);
+		this.getValidComponent();
+	}
+
+	getValidComponent() {
+		if(this.props.searchResults) {
+			this.rowComponent = <SearchResultRow data={row} />;
+			this.data = this.props.searchResults;
+		} else if(this.props.data) {
+			this.rowComponent = <SearchResultRow data={row}/>
+			this.data = this.props.data;
+		}
 	}
 
 	render() {
-		console.log(this.props.searchResults);
+		// console.log(this.props.searchResults);
 		return (
 			<div>
 				{
-					this.props.searchResults.map( (row) => {
-						return <SearchResultRow data={row} />
+					this.data.map( (row) => {
+						return this.rowComponent
 					})
 				}
 			</div>
